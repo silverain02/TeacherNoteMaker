3d65108f888df419dd32a15d79a00dea254e4411
silverain <ekdldkaa@naver.com>
2025-02-04 11:23:10 +0900

    fix : Chakra provider적용 오류 해결

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,12 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider } from "@chakra-ui/provider";
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    {/* <ChakraProvider> */}
-    <App />
-    {/* </ChakraProvider> */}
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
   </StrictMode>
 );
