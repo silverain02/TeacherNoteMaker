19da388c2065045c600a1741d0532ddf0b1a7a2b
silverain <ekdldkaa@naver.com>
2025-02-04 17:38:43 +0900

    chore : testing 컴포넌트 삭제

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,4 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import AnalyzeSyntaxComponent from "./components/AnalyzeSyntaxComponent";
 import { Routes, Route } from "react-router-dom";
 import ChatPage from "./pages/ChatPage";
 const queryClient = new QueryClient();
@@ -9,7 +8,6 @@ function App() {
       <QueryClientProvider client={queryClient}>
         <Routes>
           <Route path="/chat" element={<ChatPage />} />
-          <Route path="/" element={<AnalyzeSyntaxComponent />} />
         </Routes>
       </QueryClientProvider>
     </div>
