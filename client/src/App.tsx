b40bc12517a02cb41d3922faa2b0dcc0ca991081
silverain <ekdldkaa@naver.com>
2025-02-04 13:33:38 +0900

    feat : 라우팅 처리

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,18 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import AnalyzeSyntaxComponent from "./components/AnalyzeSyntaxComponent";
+import { Routes, Route } from "react-router-dom";
+import Chat from "./pages/Chat";
 const queryClient = new QueryClient();
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <AnalyzeSyntaxComponent />
-    </QueryClientProvider>
+    <div>
+      <QueryClientProvider client={queryClient}>
+        <Routes>
+          <Route path="/chat" element={<Chat />} />
+          <Route path="/" element={<AnalyzeSyntaxComponent />} />
+        </Routes>
+      </QueryClientProvider>
+    </div>
   );
 }
 
