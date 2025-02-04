41af3b9f9e6aef64c25c75c83fb77274f7ad07de
silverain <ekdldkaa@naver.com>
2025-02-04 10:43:03 +0900

    feat : analyzeSyntax api 모듈화 및 테스팅 컴포넌트 구현

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,10 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-
+import AnalyzeSyntaxComponent from "./components/AnalyzeSyntaxComponent";
 const queryClient = new QueryClient();
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <h1>hello world!</h1>
+      <AnalyzeSyntaxComponent />
     </QueryClientProvider>
   );
 }
